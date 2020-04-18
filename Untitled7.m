eabe770866e349b584c08918b688b3f4470a1837
B41052006 <63488613+B41052006@users.noreply.github.com>
2020-04-18 11:09:28 +0800

    Add files via upload

---
+++ Untitled7.m
@@ -0,0 +1,9 @@
+%¾Ç¸¹:B41052006
+%©m¦W:²ø§Ó½÷
+clear
+A=[123];
+B=[-1 3 -1];
+% dot
+c=dot(A,B)
+%cross
+D=cross(A,B)
---
+++ spower.m
@@ -0,0 +1,4 @@
+%¾Ç¸¹:B41052006
+%©m¦W:²ø§Ó½÷
+function p=spower(x)
+p=(norm(x)^2)/length(x);
